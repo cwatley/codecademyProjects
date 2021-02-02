516ee655faf60ed34b0b8337092315b9cb79510e
C.Watley <77905065+cwatley@users.noreply.github.com>
2021-02-02 11:15:54 -0800

    content arrays complete

--- mixed-messages/mixedMessages.js
+++ mixed-messages/mixedMessages.js
@@ -10,10 +10,11 @@ function bidAskSizeQuote() {
     } else {
         bidAsk = 'Ask';
     };
-    let size = (Math.round((Math.random() * 4)) * 100);
+    let size = (Math.round((Math.random() * 8)+1) * 100);
     let num = (Math.random() * 200)
     let quote = num.toFixed(2);
     return `${bidAsk} ${size} shares @ $${quote}`
 } 
 
-console.log(bidAskSizeQuote());
+let eightBallArray = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes--definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy, try again.', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', "Don't count on it", 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very Doubtful'];
+
